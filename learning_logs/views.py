01b1466864e21540d1728d2b7d4006a938d23ee5
dgiraldos <danielgiral2@gmail.com>
2026-06-08 10:11:23 +0200

    cambios

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
 def index(request):
     """La página de inicio para Learning Log"""
     return render(request, 'learning_logs/index.html')
