cdf05117d910609151491bee988a84d4be393618
dgiraldos <danielgiral2@gmail.com>
2026-06-09 10:50:03 +0200

    base, topic y topics html

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,4 +7,8 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Página de inicio
     path('', views.index, name='index'),
+    # Página que muestra todos los temas
+    path('topics/', views.topics, name='topics'),
+    # Página de detalles sobre un tema individual.
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
-
+from .models import Topic
 # Create your views here.
 
 def index(request):
     """La página de inicio para Learning Log"""
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Muestra todos los temas"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """Muestra un tema concreto y todas sus entradas"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
